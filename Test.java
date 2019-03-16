9d27a7f6945f5c8112213067c0dd5c13ef15a5b5
feval <940051592@qq.com>
2019-03-16 23:33:56 +0800

    练习

---
+++ Test.java
@@ -0,0 +1,18 @@
+public class Test{
+	
+	public static void main(String[] args) {
+		int[] array=new int[]{1,4,3,4,55,77,6,9,8};
+		Max(array);
+	}
+	public static int Max(int[] arr) {
+       for (int i = 1 ; i< arr.length; i++){  
+  			 int[] data=new int[9]
+	      	 sum += data[i] ;      
+	    	 if (data[i]>max){         
+	      	 max = data[i] ;     
+	        }
+	
+	}
+		
+	}
+}
