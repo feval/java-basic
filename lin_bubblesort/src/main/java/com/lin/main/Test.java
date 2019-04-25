b5b470eeebbf544c3115eb567f51116b2090900c
feval <940051592@qq.com>
2019-04-25 17:15:45 +0800

    排序_冒泡,插入,希尔

--- lin_bubblesort/src/main/java/com/lin/main/Test.java
+++ lin_bubblesort/src/main/java/com/lin/main/Test.java
@@ -20,14 +20,28 @@ public class Test {
         System.out.println(Arrays.toString(array));
     }
 
+    public static void main2(String[] args) {
+        int[] array=new int[1000];
+        for (int i = 0; i < array.length; i++) {
+            array[i]=1000-i;
+        }
+        long start = System.nanoTime();
+        TestInsertSort.insertSort(array);
+        long end = System.nanoTime();
+        System.out.println(Arrays.toString(array));
+        System.out.println(end-start);
+    }
+
     public static void main(String[] args) {
         int[] array=new int[1000];
         for (int i = 0; i < array.length; i++) {
             array[i]=1000-i;
         }
+        long start = System.nanoTime();
         TestInsertSort.insertSort(array);
+        long end = System.nanoTime();
         System.out.println(Arrays.toString(array));
+        System.out.println(end-start);
     }
-
 
 }
--- lin_bubblesort/src/main/java/com/lin/sort/TestShellSort.java
+++ lin_bubblesort/src/main/java/com/lin/sort/TestShellSort.java
@@ -5,4 +5,24 @@ package com.lin.sort;
  * Author:  llf
  */
 public class TestShellSort {
+    public static void shell(int[] array, int gap) {
+        for (int i = gap; i < array.length; i++) {
+            int j;
+            int tmp = array[i];
+            for (j = i - gap; j >= 0; j -= gap) {
+                if (tmp < array[j]) {
+                    array[j + 1] = array[j];
+                } else {
+                    break;
+                }
+            }
+            array[j+gap]=tmp;
+        }
+    }
+    public static void shellSort(int[] array) {
+        int[] drr={5,2,1};
+        for (int i = 0; i <drr.length ; i++) {
+            shell(array,drr[i]);
+        }
+    }
 }
