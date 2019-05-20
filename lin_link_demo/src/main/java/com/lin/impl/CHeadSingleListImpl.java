37d45fb5f5b948787e5bb3e015b970420e02cc71
feval <940051592@qq.com>
2019-05-20 10:41:41 +0800

    带头循环单链表1

--- lin_link_demo/src/main/java/com/lin/impl/CHeadSingleListImpl.java
+++ lin_link_demo/src/main/java/com/lin/impl/CHeadSingleListImpl.java
@@ -11,54 +11,93 @@ public class CHeadSingleListImpl implements CLinkList {
     class Node {
         private int data;
         private Node next;
+
         public Node() {
-            this.data=-1;
+            this.data = -1;
         }
+
         public Node(int data) {
-            this.data=data;
+            this.data = data;
         }
     }
 
     private Node head;
-    public CHeadSingleListImpl(){
-        this.head=new Node();
-        this.head.next=this.head;
+
+    public CHeadSingleListImpl() {
+        this.head = new Node();
+        this.head.next = this.head;
     }
+
     @Override
     public void addFirst(int data) {
-        Node node=new Node(data);
-        node.next=this.head.next;
-        this.head.next=node;
+        Node node = new Node(data);
+        node.next = this.head.next;
+        this.head.next = node;
     }
 
     @Override
     public void addLast(int data) {
-        Node cur=this.head;
-        while (cur.next!=this.head) {
-            cur=cur.next;
+        Node cur = this.head;
+        while (cur.next != this.head) {
+            cur = cur.next;
         }
-        Node node=new Node(data);
-        node.next=this.head;
-        cur.next=node;
+        Node node = new Node(data);
+        node.next = this.head;
+        cur.next = node;
     }
 
     private void checkIndex(int index) {
-        throw new IndexOutOfBoundsException("下标不合法");
+        if (index<0||index>getLength()) {
+            throw new IndexOutOfBoundsException("下标不合法");
+        }
     }
+
     @Override
     public boolean addIndex(int index, int data) {
         checkIndex(index);
-        return false;
+        Node cur=this.head;
+        for (int i = 0; i < index; i++) {
+            cur=cur.next;
+        }
+        Node node=new Node(data);
+        node.next=cur.next;
+        return true;
     }
 
     @Override
     public boolean contains(int key) {
+        Node cur=this.head.next;
+        while (cur!=this.head) {
+            if (cur.data==key) {
+                return true;
+            }
+            cur=cur.next;
+        }
         return false;
     }
 
     @Override
     public int remove(int key) {
-        return 0;
+        Node pre=searchPre(key);
+        if (pre==null) {
+            throw new UnsupportedOperationException("key不存在");
+        }
+        int oldData=0;
+        Node delNode=pre.next;
+        oldData=delNode.data;
+        pre.next=delNode.next;
+        return oldData;
+    }
+
+    private Node searchPre(int key) {
+        Node pre=this.head;
+        while (pre.next!=this.head) {
+            if (pre.next.data==key) {
+                return pre;
+            }
+            pre=pre.next;
+        }
+        return null;
     }
 
     @Override
