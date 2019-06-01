f6a3061c772faa4894d3617b3590122b0b3595a9
feval <940051592@qq.com>
2019-06-01 12:39:19 +0800

    队列和栈初始

---
+++ lin_quene_stack/src/main/java/com/lin/impl/MyQueueImpl.java
@@ -0,0 +1,30 @@
+package com.lin.impl;
+
+import com.lin.IMyQueue;
+
+/**
+ * Description:
+ * Author:  llf
+ * Created in 2019/5/28 12:33
+ */
+public class MyQueueImpl implements IMyQueue {
+    public boolean empty() {
+        return false;
+    }
+
+    public int peek() {
+        return 0;
+    }
+
+    public int poll() {
+        return 0;
+    }
+
+    public void add(int item) {
+
+    }
+
+    public int size() {
+        return 0;
+    }
+}
---
+++ lin_quene_stack/src/main/java/com/lin/impl/MyStackImpl.java
@@ -0,0 +1,30 @@
+package com.lin.impl;
+
+import com.lin.IMyStack;
+
+/**
+ * Description:
+ * Author:  llf
+ * Created in 2019/5/28 12:33
+ */
+public class MyStackImpl implements IMyStack {
+    public void push(int item) {
+        
+    }
+
+    public int pop() {
+        return 0;
+    }
+
+    public int peek() {
+        return 0;
+    }
+
+    public boolean empty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+}
